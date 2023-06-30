191885530b4a12b8d8b35b841c4fbec4e53c7085
Dharita Patel <134672624+Dharitap9803@users.noreply.github.com>
2023-06-30 12:08:44 +0530

    Add files via upload

---
+++ practical2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class practical2
+{
+	public  static void main(String args[])
+	{
+		if (args.length<5){
+			Scanner scanner=new Scanner(System.in);
+			System.out.println("Enter the name of the friends as command line arguments");
+			return;
+		}
+		String Bhavik=args[0];
+		String Sunny =args[1];
+		String Hasti =args[2];
+		String Beauty=args[3];
+		String Vidhi=args[4];
+
+		System.out.println("Greeting messages for your friends :");
+
+		System.out.println("1.Hello," +Bhavik+"!");
+		System.out.println("2,Hii,"+ Sunny + "!");
+		System.out.println("3,heyy,"+Hasti+"!");
+		System.out.println("4,howdy,"+Beauty+"!");
+		System.out.println("5,hiii there,"+Vidhi+"!");
+		
+	}
+}

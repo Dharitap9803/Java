969a384936533ebb270abaa75606fb6656362d0c
Dharita Patel <134672624+Dharitap9803@users.noreply.github.com>
2023-06-30 12:09:08 +0530

    Add files via upload

---
+++ practical3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class practical3
+{
+	public static void main(String[] args) {
+		{
+          System.out.println("Byte\t"+ Byte.SIZE+ "\t"+ Byte.MIN_VALUE +"\t +Byte.MAX_VALUE");
+          System.out.println("Short\t"+ Short.SIZE+"\t"+ Short.MIN_VALUE +"\t\t +Short.MAX_VALUE");
+          System.out.println("Integer\t"+ Integer.SIZE+"\t"+Integer.MIN_VALUE +"\t\t +Integer.MAX_VALUE");
+          System.out.println("Float\t"+ Float.SIZE+"\t"+Float.MIN_VALUE  +"\t\t +Float.MAX_VALUE");
+          System.out.println("Double\t"+ Double.SIZE+"\t"+Double.MIN_VALUE  +"\t\t  +Double.MAX_VALUE");
+          System.out.println("Character\t"+ Character.SIZE);
+      }
+	}
+}

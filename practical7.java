0c3aa6b1d5fb8b1364a0a5a2d856a80238eff9ee
Dharita Patel <134672624+Dharitap9803@users.noreply.github.com>
2023-06-30 12:10:53 +0530

    Add files via upload

---
+++ practical7.java
@@ -0,0 +1,14 @@
+
+class practical7
+{
+	public static void main(String args[])
+	{
+		int[] numbers ={2,4,6,8,10};
+
+		int sum=0;
+		for(int num: numbers){
+			sum  += num;
+		}
+        System.out.println("Sum:  "+ sum);
+	}
+}

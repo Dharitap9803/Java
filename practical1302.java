77808639240612f611cb5fc7861b3fe87c93fe60
Dharita Patel <134672624+Dharitap9803@users.noreply.github.com>
2023-07-08 12:57:54 +0530

    Add files via upload

---
+++ practical1302.java
@@ -0,0 +1,21 @@
+//Write a program which will perform following functionality on String:b)convert to lowercase 
+import java.util.Scanner;
+import java.util.*;
+
+class practical1302
+{
+	public static void main(String args[])
+	{
+		String str="HELLO WORLD!";
+
+		String strLowerCase = str.toLowerCase();
+     
+        System.out.println("Java  string to Lower Case  :"+strLowerCase);
+
+        readUserInputAndPrintInLowerCase();
+	}
+
+	private static void readUserInputAndPrintInLowerCase(){
+
+	}
+}

99e8c91c10e76280b64bdabe1dd89a0f8411a6d3
Dharita Patel <134672624+Dharitap9803@users.noreply.github.com>
2023-07-08 12:56:48 +0530

    Add files via upload

---
+++ practical1301.java
@@ -0,0 +1,21 @@
+// Write a program which will perform following functionality on String: a) convert to uppercase .
+import java.util.Scanner;
+import java.util.*;
+
+class practical1301
+{
+	public static void main(String[]  args)
+	{
+       String str ="hello World!";
+
+       String strUpperCase = str.toUpperCase();
+
+       System.out.println("Java string to Upper Case :"+ strUpperCase);
+
+       readUserInputAndPrintInUpperCase();
+	}
+
+	private static void readUserInputAndPrintInUpperCase(){
+
+	}
+}
